a9c70c6b78502ac38437d42aa1c981f2d8dd0412
Rhys Miller <rjmills87@aol.com>
2023-02-25 21:03:27 +0000

    Selectors added to all buttons.

---
+++ script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+//Button Selectors
+
+const oneBtn = document.querySelector("#btn-one");
+const twoBtn = document.querySelector("#btn-two");
+const threeBtn = document.querySelector("#btn-three");
+const fourBtn = document.querySelector("#btn-four");
+const fiveBtn = document.querySelector("#btn-five");
+const sixBtn = document.querySelector("#btn-six");
+const sevenBtn = document.querySelector("#btn-seven");
+const eightBtn = document.querySelector("#btn-eight");
+const nineBtn = document.querySelector("#btn-nine");
+const zeroBtn = document.querySelector("#btn-zero");
+
+const clearBtn = document.querySelector("#clear-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+
+const addBtn = document.querySelector("#btn-add");
+const subtractBtn = document.querySelector("#btn-subtract");
+const divideBtn = document.querySelector("#btn-divide");
+const multiplyBtn = document.querySelector("#btn-multiply");
+const equalsBtn = document.querySelector("#btn-equals");
+
+//Event Listeners
+
+oneBtn.addEventListener("click", function () {
+  console.log(1);
+});
+
+twoBtn.addEventListener("click", function () {
+  console.log(2);
+});
+
+threeBtn.addEventListener("click", function () {
+  console.log(3);
+});
+
+fourBtn.addEventListener("click", function () {
+  console.log(4);
+});
+
+fiveBtn.addEventListener("click", function () {
+  console.log(5);
+});
+
+sixBtn.addEventListener("click", function () {
+  console.log(6);
+});
+
+sevenBtn.addEventListener("click", function () {
+  console.log(7);
+});
+
+eightBtn.addEventListener("click", function () {
+  console.log(8);
+});
+
+nineBtn.addEventListener("click", function () {
+  console.log(9);
+});
+
+zeroBtn.addEventListener("click", function () {
+  console.log(0);
+});
